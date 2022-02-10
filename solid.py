c3a0149aa06b88ed7a35b4a54123a0058f3fbb3a
German <gnm3000@gmail.com>
2022-02-10 20:03:33 -0300

    flake8 fixing

--- solid.py
+++ solid.py
@@ -1,14 +1,13 @@
 
 """[D Dependency Inversion]
-We want to depends of abstraction, and not in concrete subclasses
-
-here payment processes, are depending in specific authrizers.
-Create other abstract authorizer class that you pass to payment process 
-
+# We want to depends of abstraction, and not in concrete subclasses
+# here payment processes, are depending in specific authrizers.
+# Create other abstract authorizer class that you
+# pass to payment process
 # NOW we are going to create another autorization method
-
-
     """
+
+
 from abc import ABC, abstractmethod
 
 
@@ -29,29 +28,35 @@ class Order:
             total += self.quantities[i] * self.prices[i]
         return total
 
+
 class Authorizer(ABC):
     @abstractmethod
     def is_authorized(self) -> bool:
         pass
 
+
 class SMSAuth(Authorizer):
-    
+
     authorized = False
-    
-    def verify_code(self,code):
+
+    def verify_code(self, code):
         print(f"Verify code {code}")
-        self.authorized=True
+        self.authorized = True
+
     def is_authorized(self) -> bool:
         return self.authorized
-        
+
+
 class NotARobot(Authorizer):
     authorized = False
-    
+
     def not_a_robot(self):
-        print(f" Are you a robot? Naa")
-        self.authorized=True
+        print(" Are you a robot? Naa")
+        self.authorized = True
+
     def is_authorized(self) -> bool:
         return self.authorized
+
 
 class PaymentProcessor(ABC):
     """[We create an abstract PaymentProcessor Class]
@@ -61,58 +66,52 @@ class PaymentProcessor(ABC):
     def pay(self, order):
         pass
 
-    
-
-
 
 class DebitPaymentProcessor(PaymentProcessor):
-    def __init__(self, security_code,authorizer: Authorizer) -> None:
+    def __init__(self, security_code, authorizer: Authorizer) -> None:
         self.authorizer = authorizer
         self.security_code = security_code
-        
-
-
 
     def pay(self, order):
         if(not self.authorizer.is_authorized()):
             raise Exception("Not Authorized")
 
         print("Processing debit payment type")
-        print(f"Verify security code", self.security_code)
+        print("Verify security code", self.security_code)
         order.status = "paid"
 
 
 class CreditPaymentProcessor(PaymentProcessor):
-    
+
     def __init__(self, security_code) -> None:
         self.security_code = security_code
-    
+
     def pay(self, order):
         print("Processing credit payment type")
-        print(f"Verify security code", self.security_code)
+        print("Verify security code", self.security_code)
         order.status = "paid"
 
 
 class PaypalPaymentProcessor(PaymentProcessor):
-    def __init__(self, email_address,authorizer: Authorizer) -> None:
-        self.authorizer=authorizer
+    def __init__(self, email_address, authorizer: Authorizer) -> None:
+        self.authorizer = authorizer
         self.email_address = email_address
-    
+
     def pay(self, order):
         if(not self.authorizer.is_authorized()):
             raise Exception("Not Authorized")
         print("Processing paypal payment type")
-        print(f"Verify email address", self.email_address)
+        print("Verify email address", self.email_address)
         order.status = "paid"
 
 
 class BitcoinPaymentProcessor(PaymentProcessor):
     def __init__(self, wallet) -> None:
         self.wallet = wallet
-    
+
     def pay(self, order):
         print("Processing Bitcoin payment type")
-        print(f"Verify Wallet", self.wallet)
+        print("Verify Wallet", self.wallet)
         order.status = "paid"
 
 
@@ -125,7 +124,7 @@ print(order.total_price())
 
 authorizer = NotARobot()
 
-processor = PaypalPaymentProcessor("gnm3000@gmail.com",authorizer=authorizer)
+processor = PaypalPaymentProcessor("gnm3000@gmail.com", authorizer=authorizer)
 authorizer.not_a_robot()
-# this works because we made sure that the authorizer is a tyoe of authorizer. 
+# this works because we made sure that the authorizer is a tyoe of authorizer.
 processor.pay(order)
