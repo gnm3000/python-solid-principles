d1922f24f43f9eaa152a1efe0c314688f91dbab9
German <gnm3000@gmail.com>
2022-02-11 15:04:12 -0300

    we start to apply cohesion and couping to other example

---
+++ cohesion_and_coupling.py
@@ -0,0 +1,61 @@
+"""[summary]
+
+We have a problem in application.Register_vehicle. 
+Its doing a lot of different things. So this method has low cohesion. And meny responsabilities.
+Also high copling, reply on implementation details with VehicleRegistry class. 
+If I change something in VehicleRegistry I will change Register_vechicle.
+
+1. where the information store? Where are the information expert?
+
+
+"""
+
+import string
+import random
+
+class VehicleRegistry:
+    
+    def generate_vehicle_id(self,length):
+        return ''.join(random.choices(string.ascii_uppercase,k=length))
+    
+    def generate_vehicle_license(self,id):
+        return f"{id[:2]}-{''.join(random.choices(string.digits,k=2))}-{''.join((random.choices(string.ascii_uppercase,k=2)))}"
+    
+class Application:
+    
+    def register_vehicle(self, brand: string):
+        # create registry instance
+        registry = VehicleRegistry()
+        
+        #generate a vehicle id of length 12
+        vehicle_id = registry.generate_vehicle_id(12)
+        
+        #now generate a license plate 
+        license_plate = registry.generate_vehicle_license(vehicle_id)
+        
+        #compute catalogue price
+        catalogue_price = 0
+        if brand == "Tesla Model 3":
+            catalogue_price = 60000
+        elif brand == "Volkswagen ID3":
+            catalogue_price = 35000
+        elif brand == "BMW 5":
+            catalogue_price = 45000
+        
+        # compute tax price
+        tax_percentage = 0.05
+        if brand == "Tesla Model 3" or brand == "Volkswagen ID3":
+            tax_percentage=0.02
+        
+        # compute payable tax
+        payable_tax = tax_percentage * catalogue_price
+        
+        # print out registration
+        print("Registration complete, vehicle information:")
+        print(f"Brand: {brand}")
+        print(f"ID: {vehicle_id}")
+        print(f"License plate: {license_plate}")
+        print(f"Payable tax: {payable_tax}")
+        
+app = Application()
+app.register_vehicle("Volkswagen ID3")
