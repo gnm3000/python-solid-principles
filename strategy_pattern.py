13e86ffee2b79af628175fce1b8faf0b96607563
German <gnm3000@gmail.com>
2022-02-11 21:31:03 -0300

    now we can add meny new strategies as You want

--- strategy_pattern.py
+++ strategy_pattern.py
@@ -1,11 +1,13 @@
 
+import re
 import string
 import random
 from typing import List
-
+from abc import ABC,abstractmethod
 
 def generate_id(length=8):
     return ''.join(random.choices(string.ascii_uppercase, k=length))
+
 
 
 class SupportTicket:
@@ -18,6 +20,37 @@ class SupportTicket:
         self.customer = customer
         self.issue = issue
 
+# lets create a basic strategy for us
+class TicketOrderingStrategy(ABC):
+    
+    @abstractmethod
+    def create_ordering(self,list: List[SupportTicket]) -> List[SupportTicket]:
+        pass
+    
+class FIFOrderingStrategy(TicketOrderingStrategy):
+    def create_ordering(self,list: List[SupportTicket]) -> List[SupportTicket]:
+        return list.copy()
+    
+class FILOrderingStrategy(TicketOrderingStrategy):
+    def create_ordering(self,list: List[SupportTicket]) -> List[SupportTicket]:
+        list_copy = (list.copy())
+        list_copy.reverse()
+        return list_copy
+class BlackHoleOrderingStrategy(TicketOrderingStrategy):
+    def create_ordering(self,list: List[SupportTicket]) -> List[SupportTicket]:
+        return []
+        
+    
+class RandomOrderingStrategy(TicketOrderingStrategy):
+    def create_ordering(self,list: List[SupportTicket]) -> List[SupportTicket]:
+        
+        list_copy = list.copy()
+        random.shuffle(list_copy)
+        return list_copy
+    
+    
+    
+
 
 class CustomerSupport:
 
@@ -25,25 +58,19 @@ class CustomerSupport:
 
     def create_ticket(self, customer, issue):
         self.tickets.append(SupportTicket(customer, issue))
-
-    def process_tickets(self, processing_strategy: str = "fifo"):
+        
+    def process_tickets(self, processing_strategy: TicketOrderingStrategy):
+        
+        # create ordered list
+        ticket_list = processing_strategy.create_ordering(self.tickets)
         # if its empty , dont do anything
-        if len(self.tickets) == 0:
+        if len(ticket_list) == 0:
             print("There are no tickets to process. Well done!")
             return
-
-        if(processing_strategy == "fifo"):
-            for ticket in self.tickets:
-                self.process_ticket(ticket)
-        elif(processing_strategy == "filo"):
-            for ticket in reversed(self.tickets):
-                self.process_ticket(ticket)
-        elif(processing_strategy == "random"):
-            list_copy = self.tickets.copy()
-            random.shuffle(list_copy)
-            for ticket in list_copy:
-                self.process_ticket(ticket)
-
+        
+        for ticket in ticket_list:
+            self.process_ticket(ticket)
+            
     def process_ticket(self, ticket: SupportTicket):
         print("======================================")
         print(f"Processing ticket id: {ticket.id}")
@@ -60,7 +87,7 @@ app.create_ticket("John", "My computer make strange sounds!")
 app.create_ticket("Arjan", "VSCode dont save my work")
 app.create_ticket("Linus", "I cant upload videos")
 
-app.process_tickets("filo")
+app.process_tickets(BlackHoleOrderingStrategy())
 
 # The problem with this code relay in the if statement. 
 # # If you add more strategies need to extends conditions
