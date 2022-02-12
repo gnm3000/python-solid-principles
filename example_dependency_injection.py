79a2af093923e8b48cc1704513e95b6e988096e6
German <gnm3000@gmail.com>
2022-02-12 20:37:03 -0300

    example of dependency injection - before-after

--- example_dependency_injection.py
+++ example_dependency_injection.py
@@ -2,19 +2,52 @@
 https://python-dependency-injector.ets-labs.org/introduction/di_in_python.html
 
 """
+### before
+
+import os
+
+
+class ApiClient:
+
+    def __init__(self):
+        self.api_key = os.getenv("API_KEY")  # <-- dependency
+        self.timeout = os.getenv("TIMEOUT")  # <-- dependency
+
+
+class Service:
+
+    def __init__(self):
+        self.api_client = ApiClient()  # <-- dependency
+
+
+def main() -> None:
+    service = Service()  # <-- dependency
+    ...
+
+
+if __name__ == "__main__":
+    main()
+    
+    
+### after
+
+
 import os
 
 class ApiClient:
-    def __init__(self): 
-        self.api_key = os.getenv("API_KEY",None) # <--- dependency
-        self.timeout = os.getenv("TIMEOUT",None) # <--- dependency
+    def __init__(self, api_key:str, timeout: int): 
+        self.api_key = api_key # <-- dependency injected
+        self.timeout = timeout # <-- dependency injected
         
 class Service:
     
-    def __init__(self):
-        self.api_client = ApiClient()  # <--- dependency
-def main() -> None:
-    service = Service() # <--- dependency
+    def __init__(self , api_client: ApiClient): # <-- dependency injected
+        self.api_client = api_client # <-- dependency injected
+def main(service: Service) -> None: # <-- dependency injected
+    pass
     
 if __name__=="__main__":
-    main()
+    main(Service(ApiClient(
+        api_key=os.getenv("API_KEY"),
+        timeout=os.getenv("TIMEOUT")
+    )))
