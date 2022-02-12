b04f95fdc4e7b9355b95ab4a2aa9dcdd79112084
German <gnm3000@gmail.com>
2022-02-12 16:33:50 -0300

    example of a singleton pattern in Python

---
+++ singleton_pattern.py
@@ -0,0 +1,19 @@
+class Singleton(type):
+    _instances = {}
+    
+    def __call__(cls, *args, **kwds):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton,cls).__call__(*args, **kwds)
+        return cls._instances[cls]
+    
+class Logger(metaclass=Singleton):
+    def log(self,msg):
+        print(msg)
+        
+        
+logger1 = Logger()
+logger2 = Logger()
+print(logger1)
+print(logger2)
+logger1.log("Hi")
+logger2.log("Hello")
