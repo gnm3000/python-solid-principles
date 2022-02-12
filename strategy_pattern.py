ffb43886a398d050061d524d2d60a53ed766c339
German <gnm3000@gmail.com>
2022-02-11 21:56:32 -0300

    finished strategy_pattern with functional approach

--- strategy_pattern.py
+++ strategy_pattern.py
@@ -2,7 +2,7 @@
 import re
 import string
 import random
-from typing import List
+from typing import List, Callable
 from abc import ABC,abstractmethod
 
 def generate_id(length=8):
@@ -27,42 +27,43 @@ class TicketOrderingStrategy(ABC):
     def create_ordering(self,list: List[SupportTicket]) -> List[SupportTicket]:
         pass
     
-class FIFOrderingStrategy(TicketOrderingStrategy):
-    def create_ordering(self,list: List[SupportTicket]) -> List[SupportTicket]:
-        return list.copy()
+def fifo_ordering(list: List[SupportTicket]) -> List[SupportTicket]:
+    return list.copy()
+
+def filo_ordering(self,list: List[SupportTicket]) -> List[SupportTicket]:
+    list_copy = (list.copy())
+    list_copy.reverse()
+    return list_copy
+
+def blackhole_ordering(list: List[SupportTicket]) -> List[SupportTicket]:
+    return []
     
-class FILOrderingStrategy(TicketOrderingStrategy):
-    def create_ordering(self,list: List[SupportTicket]) -> List[SupportTicket]:
-        list_copy = (list.copy())
-        list_copy.reverse()
-        return list_copy
-class BlackHoleOrderingStrategy(TicketOrderingStrategy):
-    def create_ordering(self,list: List[SupportTicket]) -> List[SupportTicket]:
-        return []
-        
     
-class RandomOrderingStrategy(TicketOrderingStrategy):
-    def create_ordering(self,list: List[SupportTicket]) -> List[SupportTicket]:
-        
-        list_copy = list.copy()
-        random.shuffle(list_copy)
-        return list_copy
+def random_ordering(list: List[SupportTicket]) -> List[SupportTicket]:
+    
+    list_copy = list.copy()
+    random.shuffle(list_copy)
+    return list_copy
     
     
     
 
 
 class CustomerSupport:
+    
 
     tickets: List[SupportTicket] = []
 
+    def __new__(cls):
+        return super(CustomerSupport,cls).__new__(cls)
+    
     def create_ticket(self, customer, issue):
         self.tickets.append(SupportTicket(customer, issue))
         
-    def process_tickets(self, processing_strategy: TicketOrderingStrategy):
+    def process_tickets(self, processing_strategy_fn : Callable[[List[SupportTicket]],List[SupportTicket]]):
         
         # create ordered list
-        ticket_list = processing_strategy.create_ordering(self.tickets)
+        ticket_list = processing_strategy_fn(self.tickets)
         # if its empty , dont do anything
         if len(ticket_list) == 0:
             print("There are no tickets to process. Well done!")
@@ -87,9 +88,11 @@ app.create_ticket("John", "My computer make strange sounds!")
 app.create_ticket("Arjan", "VSCode dont save my work")
 app.create_ticket("Linus", "I cant upload videos")
 
-app.process_tickets(BlackHoleOrderingStrategy())
+app.process_tickets(random_ordering)
 
 # The problem with this code relay in the if statement. 
 # # If you add more strategies need to extends conditions
 # so process_tickets have low cohesion. Because is responsable not only
 # tickers also for implement strategies
+
+# NOW we are going to design a functional strategies. 
